d6691c6f5d94cc059d236ead45577526c07d00af
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-11-04 19:05:10 +0200

    feat: add ScrollUp

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,12 @@ import pageNavigation from './modules/pageNavigation';
 import headerFon from '../components/header/headerFon';
 import Header from './../components/header/Header';
 import spollers from '../components/spollers/spollers';
+import ScrollUpButton from '../components/scrollUpButton/ScrollUpButton';
 
 window.addEventListener('DOMContentLoaded', () => {
   pageNavigation();
   headerFon();
   new Header();
   spollers();
+  new ScrollUpButton();
 });
